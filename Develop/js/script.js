d8b026fb1dc7b491980ef4a6073a7f15237e4860
Colleen Murray <colleenkhm@gmail.com>
2022-03-09 21:15:19 -0600

    display date and time on page

---
+++ Develop/js/script.js
@@ -0,0 +1,17 @@
+//display current date and time
+//still need to figure out how to make the time increase
+const now = moment();
+let displayDate = now.toString();
+document.getElementById(currentDay);
+currentDay.innerHTML = displayDate;
+
+// add events
+
+// change color for past/future events (if/else if/else statements for past, present, and future)
+
+
+
+
+
+
+
