608b552fd848244b2c01189a917ffc0a816c281e
Colleen Murray <colleenkhm@gmail.com>
2022-03-12 08:43:30 -0600

    updated css for hour display

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,13 +1,13 @@
 //display current date and time
 //still need to figure out how to make the time increase - using moments.js should work but its not loading
-const now = new Date(MM-YYYY);
-let displayDate = now.toString();
+const now = new Date();
+var dateNoTime = (now.getMonth()+1)+'/'+now.getDate()+'/'+now.getFullYear();
+let displayDate = dateNoTime.toString();
 document.getElementById(currentDay);
 currentDay.innerHTML = displayDate;
 
-// add events
+// onclick function that enables text input for each field
 // refer to taskinator for adding events/saving in local storage
-
 // change color for past/future events (if/else if/else statements for past, present, and future)
 
 
