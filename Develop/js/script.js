f86d58335e6040cbce157490a1b6484dfabbc693
Colleen Murray <colleenkhm@gmail.com>
2022-03-12 08:07:46 -0600

    time blocks wireframe

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,11 +1,12 @@
 //display current date and time
-//still need to figure out how to make the time increase
-const now = moment();
+//still need to figure out how to make the time increase - using moments.js should work but its not loading
+const now = new Date(MM-YYYY);
 let displayDate = now.toString();
 document.getElementById(currentDay);
 currentDay.innerHTML = displayDate;
 
 // add events
+// refer to taskinator for adding events/saving in local storage
 
 // change color for past/future events (if/else if/else statements for past, present, and future)
 
