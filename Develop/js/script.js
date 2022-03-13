c93ed7aa272ba2affc992c36f2aa9491c4c0d63e
Colleen Murray <colleenkhm@gmail.com>
2022-03-13 18:03:06 -0500

    update CSS to reflect example layout

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -6,11 +6,22 @@ let displayDate = dateNoTime.toString();
 document.getElementById(currentDay);
 currentDay.innerHTML = displayDate;
 
+// const now = moment().format('dddd')+moment().format('MMMM Do YYYY')
+// let displayDate = now.toString();
+// document.getElementById(currentDay)
+// currentDay.innerHTML = displayDate;
+
+// need to have number of hours as separate div or maybe undisplayed id that corresponds to time in format that javascript can read
+// need to account for am vs pm
+// if/else statements that set current time = hour fetched from new date
+// if/else must also contain addClass="hidden" and removeClass "hidden" for past, present, and future CSS styles
+// create textboxes that take up certain width of page, need to have container so that display flex doesn't set all p tags next to each other and textboxes below
+
 // onclick function that enables text input for each field
 // refer to taskinator for adding events/saving in local storage
 // change color for past/future events (if/else if/else statements for past, present, and future)
 
-
+// create a table for the different times and events
 
 
 
