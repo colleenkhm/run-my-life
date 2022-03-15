ba9b4a49742f62ee01101827dcd6d05ebbebdf6a
Colleen Murray <colleenkhm@gmail.com>
2022-03-14 19:13:56 -0500

    edited for loop to call each element in the arrray starting at the time it represents instead of at index 0

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,43 +2,50 @@
 var displayDate = moment().format('dddd MMMM Do');
 currentDay.innerHTML = displayDate;
 
-const timeBlocks = document.getElementsByClassName("timeBlock");
-let currentHour = parseInt(moment().format("HH"));
-
-var timeBlockArr = [timeBlocks]
-
+const timeBlockArr = document.getElementsByClassName("time-block");
+let currentHour = parseInt(moment().format("H"));
+console.log(timeBlockArr);
 // change color depending on whether event is in past, present, or future
- Array.from(timeBlockArr).forEach(timeBlock => {
-for (i = 0; i < timeBlockArr.length; i++) {
-    if ((timeBlockArr[i] + 8) < currentHour) {
-        element.classList.add("past");
-    } else if ((timeBlockArr[i] + 8) === currentHour) {
-        element.classList.add("present");
-    } else if ((timeBlockArr[i] + 8) > currentHour) {
-        element.classList.add("future");
+var changeColor = function() {
+    var count = 0;
+for (i = 8; i < 18; i++) {
+    console.log(timeBlockArr[count].id)
+    if (parseInt(timeBlockArr[count].id) < currentHour) {
+        Element.classList.add("past");
+    } else if (parseInt(timeBlockArr[count].id) === currentHour) {
+        Element.classList.add("present");
+    } else if (parseInt(timeBlockArr[count].id) > currentHour) {
+        Element.classList.add("future");
     }
-}});
-// change color depending on whether event is in past, present, or future
+    count ++;
+}};
 
-Array.from(timeBlocks).forEach(timeBlock => {
-    // still need to set timeBlock id equal to timeBlock - maybe I'm overcomplicating this and can do it in less code?
-    let
-        timeBlockIdString = timeBlock.id,
-        timeBlockHour;
-    if (timeBlockIdString) {
-        timeBlockHour = parseInt(timeBlockIdString);
-    }
-    if (timeBlockHour) {
-    //compares id to current hour and changes color
-        if (currentHour === timeBlockHour) {
-            timeBlock.classList.add("present");
-        } else if (currentHour < timeBlockHour){
-            timeBlock.classList.add("future");
-        } else if (currentHour > timeBlockHour) {
-            timeBlock.classList.add("future")
-        }
+changeColor();
+
+// timeBlockArr.forEach(changeColor);
+// change color depending on whether event is in past, present, or future
+// other format (after if and else/if statements):
+// ("timeBlock").css("background-color", "green");
+
+// Array.from(timeBlocks).forEach(timeBlock => {
+//     // still need to set timeBlock id equal to timeBlock - maybe I'm overcomplicating this and can do it in less code?
+//     let
+//         timeBlockIdString = timeBlock.id,
+//         timeBlockHour;
+//     if (timeBlockIdString) {
+//         timeBlockHour = parseInt(timeBlockIdString);
+//     }
+//     if (timeBlockHour) {
+//     //compares id to current hour and changes color
+//         if (currentHour === timeBlockHour) {
+//             timeBlock.classList.add("present");
+//         } else if (currentHour < timeBlockHour){
+//             timeBlock.classList.add("future");
+//         } else if (currentHour > timeBlockHour) {
+//             timeBlock.classList.add("future")
+//         }
     
-    }});
+//     }});
 
 
 // save events to local storage
