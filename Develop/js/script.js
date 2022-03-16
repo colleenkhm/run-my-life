429eba0bf4c77fd6814ef4831482ea42f5c45f34
Colleen Murray <colleenkhm@gmail.com>
2022-03-15 19:57:54 -0500

    updated javascript with jquery functions for efficiency and ease

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,57 +1,42 @@
-//display current date and time
-var displayDate = moment().format('dddd MMMM Do');
-currentDay.innerHTML = displayDate;
-
-const timeBlockArr = document.getElementsByClassName("time-block");
-let currentHour = parseInt(moment().format("H"));
-console.log(timeBlockArr);
-// change color depending on whether event is in past, present, or future
-var changeColor = function() {
-    var count = 0;
-for (i = 8; i < 18; i++) {
-    console.log(timeBlockArr[count].id)
-    if (parseInt(timeBlockArr[count].id) < currentHour) {
-        Element.classList.add("past");
-    } else if (parseInt(timeBlockArr[count].id) === currentHour) {
-        Element.classList.add("present");
-    } else if (parseInt(timeBlockArr[count].id) > currentHour) {
-        Element.classList.add("future");
-    }
-    count ++;
-}};
-
-changeColor();
-
-// timeBlockArr.forEach(changeColor);
-// change color depending on whether event is in past, present, or future
-// other format (after if and else/if statements):
-// ("timeBlock").css("background-color", "green");
-
-// Array.from(timeBlocks).forEach(timeBlock => {
-//     // still need to set timeBlock id equal to timeBlock - maybe I'm overcomplicating this and can do it in less code?
-//     let
-//         timeBlockIdString = timeBlock.id,
-//         timeBlockHour;
-//     if (timeBlockIdString) {
-//         timeBlockHour = parseInt(timeBlockIdString);
-//     }
-//     if (timeBlockHour) {
-//     //compares id to current hour and changes color
-//         if (currentHour === timeBlockHour) {
-//             timeBlock.classList.add("present");
-//         } else if (currentHour < timeBlockHour){
-//             timeBlock.classList.add("future");
-//         } else if (currentHour > timeBlockHour) {
-//             timeBlock.classList.add("future")
-//         }
+$(document).ready(function(){
+    //display current date and time
+    var displayDate = moment().format('dddd MMMM Do');
+    currentDay.innerHTML = displayDate;
+    // get save buttons to save content to text area when clicked
+    $(".saveBtn").on("click", function(){
+        // capture values inside text area and corresponding time
+        var task = $(this).siblings("textarea").val();
+        console.log(task)
+        var time = $(this).parent().attr("id");
+        console.log(time);
+        localStorage.setItem(time, task)
+    })
+    // grab items from local storage to put into text areas
+    $("#8 .description").val(localStorage.getItem("8"))
+    $("#9 .description").val(localStorage.getItem("9"))
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
     
-//     }});
+    // change color depending on whether event is in past, present, or future
+    let currentHour = parseInt(moment().format("H"));
 
+    $(".description").each(function(){
+        var timeEl = $(this).parent().attr("id");
+        console.log(timeEl)
+        if (timeEl < currentHour) {
+            $(this).addClass("past")
+        } else if (timeEl === currentHour) {
+            $(this).addClass("present")
+        } else if (timeEl > currentHour) {
+            $(this).addClass("future")
+        }
 
-// save events to local storage
-
-
-
-
-
+    })
+});
 
